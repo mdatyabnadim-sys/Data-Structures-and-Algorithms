bda8e998f4a337a4c789aacd140ed7f03363589a
mdatyabnadim <mdatyabnadim@gmail.com>
2025-10-11 00:58:14 +0530

    Array permutation added

---
+++ Array/Arraypermutation.java
@@ -0,0 +1,10 @@
+class Arraypermutation {
+    public int[] buildArray(int[] nums) {
+        int n=nums.length;
+        int[] ans = new int[n];
+        for(int i=0;i<n;i++){
+            ans[i]=nums[nums[i]];
+        }
+           return ans;
+    }
+}
