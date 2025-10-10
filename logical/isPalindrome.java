fa43ab71154a705846c74986dc56389a639af0f5
mdatyabnadim <mdatyabnadim@gmail.com>
2025-10-10 15:14:11 +0530

    4 programs updated

---
+++ logical/isPalindrome.java
@@ -0,0 +1,24 @@
+    isPalindromeoolean isPalindrome(int x) {
+        int rev=0;
+        int original=x;
+        if(x>=0){
+        while(x!=0){
+            rev*=10;
+            rev=rev+(x%10);
+            x=x/10;
+        }
+        if(original == rev){
+            return true;
+        }
+        else{
+            return false;
+        }
+        }
+        else{
+            return false;
+        }
+        
+    }
+
+    
+
