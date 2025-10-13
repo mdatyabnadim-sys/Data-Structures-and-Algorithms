11c266f0f9218b6ced3222f120c3b5116bdb2473
mdatyabnadim <mdatyabnadim@gmail.com>
2025-10-13 12:42:48 +0530

    comments added

--- Array/buysellstock.java
+++ Array/buysellstock.java
@@ -1,15 +1,16 @@
 public class buysellstock {
     public int maxprofit(int[] prices){
-    int minprice=Integer.MAX_VALUE;
-    int maxprofit=0;
+    int minprice=Integer.MAX_VALUE; // Start with very high minPrice
+    int maxprofit=0; // Start with 0 profit
     for(int i=0;i<prices.length;i++){
-        int price=prices[i];
-        if(minprice>price) minprice=price;
+        int price=prices[i]; // current day's price
+        if(minprice>price) minprice=price; // Found a lower price to buy
         else {
-            int profit =price-minprice;
-            if (profit>maxprofit) maxprofit=profit;
+            int profit =price-minprice; // What if we sold today?
+            if (profit>maxprofit) maxprofit=profit; // Update if it's the best so far
         }
     }
         return maxprofit;
     }
 }
+
