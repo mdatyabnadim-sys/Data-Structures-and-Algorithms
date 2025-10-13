b8e8f95de8fce9130ec809e0ff50e362e2e56192
mdatyabnadim <mdatyabnadim@gmail.com>
2025-10-14 00:02:13 +0530

    single sum added

---
+++ singlesum.java
@@ -0,0 +1,15 @@
+public class singlesum {
+        public int addDigits(int num) {
+        int sum=0;
+        while(num>9){
+        sum=0;
+        while(num!=0){
+            int d=num%10;
+            sum+=d;
+            num/=10;
+        }
+        num=sum;
+        }
+        return num;
+    }
+}
