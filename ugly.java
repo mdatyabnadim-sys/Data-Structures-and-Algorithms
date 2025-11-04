fce71442a328d6e5dcfbfcc33ba6638bbd5f4d99
mdatyabnadim <mdatyabnadim@gmail.com>
2025-11-05 00:14:05 +0530

    Ugly Number added

---
+++ ugly.java
@@ -0,0 +1,9 @@
+class ugly{
+    public boolean isUgly(int n) {
+    while(n>0&&n%2==0) n/=2;
+    while(n>0&&n%3==0) n/=3;
+    while(n>0&&n%5==0) n/=5;
+    if(n==1) return true;
+    else return false;
+    }
+}
