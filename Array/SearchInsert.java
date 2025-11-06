d2cda1ada4757203068417cc51ab1ef93a39e071
MD ATYAB NADIM <mdatyabnadim@gmail.com>
2025-11-07 01:01:56 +0530

    Add files via upload

---
+++ Array/SearchInsert.java
@@ -0,0 +1,14 @@
+class SearchInsert{
+    public int searchInsert(int[] nums, int target) {
+        int n=nums.length;
+        int right=n-1;
+        int left=0;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]<target) left=mid+1;
+            else right=mid-1;
+        }
+      return left;
+    }
+}
