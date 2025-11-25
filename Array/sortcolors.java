199d515a1d499ab899a3b8f4141cd738c0db7d11
MD ATYAB NADIM <mdatyabnadim@gmail.com>
2025-11-25 11:59:32 +0530

    Add files via upload

---
+++ Array/sortcolors.java
@@ -0,0 +1,23 @@
+class sortcolors{
+    public void sortColors(int[] nums) {
+        int red = 0, white = 0, blue = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0)
+                red++;
+            else if (nums[i] == 1)
+                white++;
+            else
+                blue++;
+        }
+        white += red;
+        blue += white;
+        for (int i = 0; i < nums.length; i++) {
+            if (i < red)
+                nums[i] = 0;
+            else if (i >= red && i < white)
+                nums[i] = 1;
+            else
+                nums[i] = 2;
+        }
+    }
+}
