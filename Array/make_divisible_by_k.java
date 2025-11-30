3309bc238b8b2a9667c0735c87407aedcf41c221
MD ATYAB NADIM <mdatyabnadim@gmail.com>
2025-11-30 10:28:33 +0530

    Add files via upload

---
+++ Array/make_divisible_by_k.java
@@ -0,0 +1,9 @@
+class make_divisible_by_k{
+    public int minOperations(int[] nums, int k) {
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+        }
+        return sum%k;
+    }
+}
