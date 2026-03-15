7386df488a73cfe9d89b48dec5d3be146ad0cff5
MD ATYAB NADIM <mdatyabnadim@gmail.com>
2026-03-15 22:11:43 +0530

    Add files via upload

---
+++ Array/PascalTriangle119.java
@@ -0,0 +1,13 @@
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<List<Integer>> ans=new ArrayList<>();
+        for(int i=0;i<=rowIndex;i++){
+            ans.add(new ArrayList<>());
+            for(int j=0;j<=i;j++){
+                if(j==0 || j==i) ans.get(i).add(1); // arr[i][j]=1;
+                else ans.get(i).add(ans.get(i-1).get(j)+ans.get(i-1).get(j-1));
+            }
+        }
+        return ans.get(rowIndex);
+    }
+}
